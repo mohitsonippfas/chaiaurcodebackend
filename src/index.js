654f41d9e7bcb5741e92b8bac1d1d761d163adf2
Mohit Soni <mohitsoni@ppfas.com>
2024-01-05 14:30:39 +0530

    db connectin setting

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env ",
+});
+connectDB();
+
+// First approch
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error
+    });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+})();
+*/
